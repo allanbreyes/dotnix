f399833d5cfc829ba082db46657930319aac02ac
Allan Reyes <allanbreyes@users.noreply.github.com>
2022-02-20 19:36:07 -0500

    Update configs, especially git

--- home.nix
+++ home.nix
@@ -8,11 +8,17 @@ in {
       "vimrc".source = ./files/.vimrc;
     };
     homeDirectory = "${local.usersDirectory}/${local.username}";
+    packages = with pkgs; [
+      htop
+      kubectl
+    ];
     stateVersion = "21.05";
     username = "${local.username}";
   };
 
   programs = {
+    home-manager.enable = true;
+
     exa = {
       enable = true;
       enableAliases = true;
@@ -20,8 +26,48 @@ in {
     fzf = {
       enable = true;
       enableZshIntegration = true;
+      tmux.enableShellIntegration = true;
     };
-    home-manager.enable = true;
+    git = {
+      enable = true;
+      aliases = {
+        cm = "commit -m";
+        co = "checkout";
+        lg = "log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative";
+        st = "status";
+        undo = "reset --soft HEAD^";
+        unstage = "reset HEAD --";
+      };
+      extraConfig = {
+        apply.whitespace = "nowarn";
+        color = {
+          diff = "auto";
+          status = "auto";
+          branch = "auto";
+          ui = "auto";
+        };
+        credential.helper = "cache";
+        help.autocorrect = 1;
+        init.defaultBranch = "main";
+        pull.ff = "only";
+        push.default = "simple";
+        url = {
+          "git@github.com" = { insteadOf = "gh:"; };
+        };
+      };
+      ignores = [
+        "*.swp"
+        "*~"
+        "#*"
+        ".DS_Store"
+      ];
+      signing = {
+        key = "${local.gpgSigningKey}";
+        signByDefault = true;
+      };
+      userName = "${local.fullName}";
+      userEmail = "${local.githubUsername}@users.noreply.github.com";
+    };
     starship = {
       enable = true;
       enableZshIntegration = true;
@@ -48,11 +94,17 @@ in {
       enable = true;
       enableAutosuggestions = true;
       enableCompletion = true;
+      history.extended = true;
       initExtra = ''
         bindkey -v
       '';
+      sessionVariables = {
+        EDITOR = "vim";
+      };
       shellAliases = {
         ips = "ifconfig | grep -E 'inet ' | awk '{print $2}' | grep -v '127.0.0.1' && curl http://ifconfig.co";
+        k = "kubectl";
+        switch = "home-manager switch && source ~/.zshrc";
       };
     };
   };
--- local.example.nix
+++ local.example.nix
@@ -1,4 +1,7 @@
 {
-  usersDirectory = "/home";
+  fullName = "Allan Reyes";
+  githubUsername = "allanbreyes";
+  gpgSigningKey = "3C5663F773417707";
   username = "allan";
+  usersDirectory = "/home";
 }
