7b0951fa02d33e2a4b726ef23d061b51cbd697d5
Allan Reyes <allanbreyes@users.noreply.github.com>
2022-03-13 12:35:03 -0400

    Update linux config

--- linux.nix
+++ linux.nix
@@ -76,11 +76,15 @@ in {
 
   nix = {
     allowedUsers = ["${vars.username}"];
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
     gc = {
       automatic = true;
       dates = "daily";
       options = "--delete-older-than 30d";
     };
+    package = pkgs.nixUnstable;
   };
 
   nixpkgs.config = import ./config.nix;
