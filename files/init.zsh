12249e0752c8635aa268aacd4147abec81238695
Allan Reyes <allan@vanta.com>
2022-11-04 21:15:47 -0400

    Fix partial lines

--- files/init.zsh
+++ files/init.zsh
@@ -10,6 +10,10 @@ export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
 
+# Fix partial lines
+setopt PROMPT_CR
+setopt PROMPT_SP
+
 # macOS
 if [[ `uname` == "Darwin" ]]; then
   alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
